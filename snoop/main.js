a5221634ecb54ce77d4d410f94b195ac9210e855
Sahal <aha334@nyu.edu>
2024-02-07 06:23:14 -0500

    updated startpage with working button

--- snoop/main.js
+++ snoop/main.js
@@ -1,5 +1,38 @@
 import './style.css'
+let startButton = document.getElementById('startbutton');
+var bouncyArray = [];
+
+startButton.addEventListener("click", launch);
+
+class Bouncy{
+  constuctor(){
+    this.el;
+    this.xPos;
+    this.yPos;
+    this.speed;
+    this.xDirection = 1;
+    this.yDirection = 1;
+  }
+}
 
 function launch(){
-  
+  console.log("launching")
+
+  //fade out
+ var interval = setInterval(() => {
+      var opacity = startButton.style.opacity;
+      if (opacity > 0) {
+         opacity -= 0.1;
+         startButton.style.opacity = opacity;
+      }  else {
+        clearInterval(interval);
+        startButton.style.display = 'none';
+     }
+   }, 2000);
+
+  for(i = 0; i < 18; i++){
+    var bouncy = new Bouncy();
+    bouncy.el = document.createElement('img');
+    bouncyArray.push(bouncy);
+  }
 }
